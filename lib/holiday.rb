98d86d654eb8f87ef7feaeef363b935f6d56a823
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-19 13:45:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       if supplies.include?("BBQ")
-        bbq_array << holidays
+        bbq_array << holiday
       end
     end
   end
